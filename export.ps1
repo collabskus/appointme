26968235176c6d5368d8e1ec33f3c3088b005ad2
kushal <collabskus@gmail.com>
2026-06-11 14:24:25 -0400

    add all files

--- export.ps1
+++ export.ps1
@@ -61,7 +61,7 @@ $IncludeExtensions = @(
     "cs", "json", "xml", "csproj", "slnx", "sln", "config",
     "cshtml", "razor", "js", "css", "scss", "html",
     "yml", "yaml", "sql", "props", "targets", "sh",
-    "ps1", "md"
+    "ps1", "md", "ts", "json"
 )
 
 # Exact filenames (no extension match needed)
